fba9af1a2e090fdcf6aaf5da9fc9fd13d7d538f7
Aurelie <josserand.n.aurelie@gmail.com>
2025-10-27 12:06:32 -0400

    creates XR store and wraps experience

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import { createXRStore, XR } from "@react-three/xr";
+
+const store = createXRStore();
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <>
+    <button onClick={() => store.enterVR()}>Enter VR</button>
+    <button onClick={() => store.enterAR()}>Enter AR</button>
+    <Canvas
+      flat
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [1, 2, 6],
+      }}
+    >
+      <XR store={store}>
+        <Experience />
+      </XR>
+    </Canvas>
+  </>
+);
